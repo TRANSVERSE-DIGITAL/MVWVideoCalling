eb51eb02bd426523dae1619b784c779f37ea2538
jokernifty <krishna@transversedigital.com>
2024-07-16 13:29:42 +0530

    Changes for appwrite

--- hooks/useGetCalls.ts
+++ hooks/useGetCalls.ts
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react';
-import { useUser } from '@clerk/nextjs';
+import { Client, Account } from 'appwrite';
 import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
 
 export const useGetCalls = () => {
-  const { user } = useUser();
+  const [calls, setCalls] = useState<Call[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const client = useStreamVideoClient();
-  const [calls, setCalls] = useState<Call[]>();
-  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const appwriteClient = new Client()
+          .setEndpoint('https://appwrite.xeve.dev/v1') // Your API Endpoint
+          .setProject('mvvideocall'); // Your project ID
+
+        const account = new Account(appwriteClient);
+        const user = await account.get();
+        setUser(user.$id);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   useEffect(() => {
     const loadCalls = async () => {
-      if (!client || !user?.id) return;
-      
+      if (!client || !user) return;
+
       setIsLoading(true);
 
       try {
@@ -21,8 +39,8 @@ export const useGetCalls = () => {
           filter_conditions: {
             starts_at: { $exists: true },
             $or: [
-              { created_by_user_id: user.id },
-              { members: { $in: [user.id] } },
+              { created_by_user_id: user },
+              { members: { $in: [user] } },
             ],
           },
         });
@@ -35,18 +53,20 @@ export const useGetCalls = () => {
       }
     };
 
-    loadCalls();
-  }, [client, user?.id]);
+    if (user) {
+      loadCalls();
+    }
+  }, [client, user]);
 
   const now = new Date();
 
-  const endedCalls = calls?.filter(({ state: { startsAt, endedAt } }: Call) => {
-    return (startsAt && new Date(startsAt) < now) || !!endedAt
-  })
+  const endedCalls = calls.filter(({ state: { startsAt, endedAt } }) => {
+    return (startsAt && new Date(startsAt) < now) || !!endedAt;
+  });
 
-  const upcomingCalls = calls?.filter(({ state: { startsAt } }: Call) => {
-    return startsAt && new Date(startsAt) > now
-  })
+  const upcomingCalls = calls.filter(({ state: { startsAt } }) => {
+    return startsAt && new Date(startsAt) > now;
+  });
 
-  return { endedCalls, upcomingCalls, callRecordings: calls, isLoading }
+  return { endedCalls, upcomingCalls, callRecordings: calls, isLoading };
 };
